f30e4ca797e0ad499e5be9b3bc6b471af082e461
Khalid Shakeel <khalidshakeel46@gmail.com>
2023-08-28 11:49:20 +0530

    Looping through an array in JSX to display newsItems from state

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,52 +1,6 @@
 import React, { Component } from "react";
 
 export class NewsItem extends Component {
-  articles = [
-    {
-      status: "ok",
-      totalResults: 2,
-      source: {
-        id: "espn-cric-info",
-        name: "ESPN Cric Info",
-      },
-      author: null,
-      title:
-        "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-      description:
-        "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-      url: "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-      urlToImage:
-        "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-      publishedAt: "2020-04-27T11:41:47Z",
-      content:
-        "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]",
-    },
-    {
-      source: {
-        id: "espn-cric-info",
-        name: "ESPN Cric Info",
-      },
-      author: null,
-      title:
-        "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-      description:
-        "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-      url: "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-      urlToImage:
-        "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-      publishedAt: "2020-03-30T15:26:05Z",
-      content:
-        "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]",
-    },
-  ];
-  constructor() {
-    super();
-    this.state = {
-      articles: this.articles,
-      loading: false,
-    };
-  }
-
   render() {
     let { title, description, imageUrl, newsUrl } = this.props;
     return (
@@ -54,16 +8,10 @@ export class NewsItem extends Component {
         <div className="card" style={{ width: "18rem" }}>
           <img src={imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">
-              {description} {newsUrl}
-            </p>
-            <a
-              href="https://www.thenews.com.pk/latest/1103914-pak-vs-afg-changes-in-pakistan-lineup-likely-ahead-of-third-odi"
-              className="btn btn-sm btn-primary"
-              target="blank"
-            >
-              Check Score
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <a href={newsUrl} className="btn btn-sm btn-primary" target="blank">
+              Read more
             </a>
           </div>
         </div>
