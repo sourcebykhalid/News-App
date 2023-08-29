f0daea5439cd9e0ecb9002f015690f077a15eb31
Khalid Shakeel <khalidshakeel46@gmail.com>
2023-08-29 15:54:51 +0530

    Loading method added to the News Portal to show while there is slow page loading

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
+import Spinner from "./Spinner.js";
 
 export class News extends Component {
   constructor() {
@@ -10,48 +11,54 @@ export class News extends Component {
       page: 1,
     };
   }
-
   async componentDidMount() {
-    let url =
-      "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=66346d8eb54a4edebf2cd25cb71f7639&page=1&pagesize=5";
+    let url = ` https://newsapi.org/v2/everything?domains=wsj.com&apiKey=66346d8eb54a4edebf2cd25cb71f7639&page=1&pagesize=${this.props.pageSize}`;
+    this.setState({ loading: true });
     let data = await fetch(url);
     let parseData = await data.json();
     this.setState({
       articles: parseData.articles,
       totalResults: parseData.totalResults,
+      loading: false,
     });
   }
   handlePrevClick = async () => {
     let url = `https://newsapi.org/v2/everything?domains=wsj.com&apiKey=66346d8eb54a4edebf2cd25cb71f7639&page=${
       this.state.page - 1
-    }&pagesize=5`;
-
+    }&pagesize=${this.props.pageSize}`;
+    this.setState({ loading: true });
     let data = await fetch(url);
     let parseData = await data.json();
 
     this.setState({
       page: this.state.page - 1,
       articles: parseData.articles,
+      loading: false,
     });
   };
   handleNextClick = async () => {
-    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 5)) {
-    } else {
-      let url = `https://newsapi.org/v2/everything?domains=wsj.com&apiKey=66346d8eb54a4edebf2cd25cb71f7639&page=${
-        this.state.page + 1
-      }&pagesize=5`;
-      let data = await fetch(url);
-      let parseData = await data.json();
-      this.setState({
-        page: this.state.page + 1,
-        articles: parseData.articles,
-      });
+    if (
+      !this.state.page + 1 >
+      Math.ceil(this.state.totalResults / this.props.pageSize)
+    ) {
     }
+    let url = `https://newsapi.org/v2/everything?domains=wsj.com&apiKey=66346d8eb54a4edebf2cd25cb71f7639&page=${
+      this.state.page + 1
+    }&pagesize=${this.props.pageSize}`;
+    this.setState({ loading: true });
+    let data = await fetch(url);
+    let parseData = await data.json();
+    this.setState({
+      page: this.state.page + 1,
+      articles: parseData.articles,
+      loading: false,
+    });
   };
   render() {
     return (
       <div className="container my-3">
         <h1 className="text-center">News CaFe - Global news at one place</h1>
+        {this.state.loading && <Spinner />}
         <div className="row">
           {this.state.articles.map((element) => {
             return (
